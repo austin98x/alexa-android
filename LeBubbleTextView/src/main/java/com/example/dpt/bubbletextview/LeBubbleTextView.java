305c19e5e8b2fd8071565a9c18cb38b973a56861
austin98x <wcfds@sina.com>
2018-02-22 11:04:11 +0800

    add Help Activity

---
+++ LeBubbleTextView/src/main/java/com/example/dpt/bubbletextview/LeBubbleTextView.java
@@ -0,0 +1,73 @@
+package com.example.dpt.bubbletextview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.TextView;
+
+
+/**
+ * Created by dupengtao on 15/7/25.
+ * <p/>
+ * <declare-styleable name="LeBubbleTextView">
+ * <!-- Corner radius for LeBubbleTextView. -->
+ * <attr name="bubbleCornerRadius" format="dimension"/>
+ * <!-- Background color for LeBubbleTextView. -->
+ * <attr name="bubbleBackgroundColor" format="color"/>
+ * <!-- text size for LeBubbleTextView. -->
+ * <attr name="bubbleTextSize" format="dimension"/>
+ * <!-- text color for LeBubbleTextView. -->
+ * <attr name="bubbleTextColor" format="color"/>
+ * <!-- text for LeBubbleTextView. -->
+ * <attr name="bubbleText" format="string"/>
+ * <p/>
+ * <!-- direction for arrow. -->
+ * <attr name="bubbleArrowDirection">
+ * <enum name="left" value="1"/>
+ * <enum name="top" value="2"/>
+ * <enum name="right" value="3"/>
+ * <enum name="bottom" value="4"/>
+ * </attr>
+ * <!-- direction for arrow. -->
+ * <attr name = "relativePosition" format = "fraction" />
+ * <!-- Press state Background color for LeBubbleTextView. -->
+ * <attr name="bubbleBackgroundPressColor" format="color"/>
+ * </declare-styleable>
+ */
+public class LeBubbleTextView extends LeBubbleView implements Runnable {
+
+    private TextView tvContent;
+
+    public LeBubbleTextView(Context context) {
+        super(context);
+    }
+
+    public LeBubbleTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LeBubbleTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void initChildView(float radius, int backgroundColor, int textColor, float textSize, String content) {
+        super.initChildView(radius, backgroundColor, textColor, textSize, content);
+        tvContent = new TextView(mContext);
+        tvContent.setId(View.generateViewId());
+        tvContent.setTextColor(textColor);
+        tvContent.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+        tvContent.setText(content);
+        int px22 = dip2px(21);
+        int px16 = dip2px(15);
+        tvContent.setPaddingRelative(px22, px16, px22, px16);
+        conRl.addView(tvContent);
+    }
+
+    public TextView getContentTextView(){
+        return tvContent;
+    }
+
+
+}
---
+++ app/src/main/java/com/austin/alexa/HelpActivity.java
@@ -0,0 +1,13 @@
+package com.austin.alexa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
